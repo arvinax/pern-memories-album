74119597f0d18ae95a12a84fb74d452a5d6d626a
justimnax <justimnazu@gmail.com>
2022-08-16 18:52:08 +0430

    connect to postgres via prisma and create memory

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,20 +1,30 @@
 import PostMessage from '../models/postMessage.js';
+import prisma from '../prisma/client.js';
 
+// get all posts
 export const getPosts = async (req, res) => {
   try {
-    const postMessages = await PostMessage.find();
+    const postMessages = await prisma.posts.findMany();
     res.status(200).json(postMessages);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
 };
 
+// create post
 export const createPost = async (req, res) => {
   const post = req.body;
 
-  const newPost = new PostMessage(post);
+  const newPost = await prisma.posts.create({
+    data: {
+      title: post.title,
+      message: post.message,
+      creator: post.creator,
+      selectedFile: post.selectedFile,
+      tags: post.tags,
+    },
+  });
   try {
-    newPost.save();
     res.status(201).json(newPost);
   } catch (error) {
     res.status(409).json({ message: error.message });
--- server/index.js
+++ server/index.js
@@ -11,14 +11,15 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb://0.0.0.0:27017/posts';
-const PORT = process.env.PORT || 5000;
+// const CONNECTION_URL = 'mongodb://0.0.0.0:27017/posts';
+// const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(CONNECTION_URL)
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server runnig on port ${PORT}`))
-  )
-  .catch((err) => console.log('failed', err));
+// mongoose
+//   .connect(CONNECTION_URL)
+//   .then(() =>
+//     app.listen(PORT, () => console.log(`Server runnig on port ${PORT}`))
+//   )
+//   .catch((err) => console.log('failed', err));
 
 // mongoose.set('useFindAndModify', false);
+app.listen(5000, console.log('connected to 5000'));
